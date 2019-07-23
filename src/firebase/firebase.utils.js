dd6dc13d4a1f858ea34d3a1474ed3ce3aa867358
Charlemagne <neoregaf@gmail.com>
2019-07-23 08:21:48 -0400

    added sign in and sign up with email

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,32 @@ const config = {
     appId: "1:127793472990:web:ad4acc843d096f84"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (err) {
+            console.log('error creating user', err.message);
+        }
+    }
+
+    return userRef;
+}
 
 firebase.initializeApp(config);
 
